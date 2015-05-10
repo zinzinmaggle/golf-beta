2a6e52911fb894d8febe3c41358088e973174c17
Fred <frederic.quemper@edu.esiee.fr>
2015-05-10 17:24:35 +0200

    Edit user's profile

--- client/templates/Profil/profile_submit.js
+++ client/templates/Profil/profile_submit.js
@@ -1,31 +1,28 @@
-/*Template.ProfileForm.helpers({
-  username: function() {
-    var ph = document.createElement('placeholder');
-    if()
-    {
-    	ph.placeholder = this.url;
-    }
-    else
-    {
-    	ph.placeholder = this.url;
-    }
-    return ph.placeholder;
-  }
-});*/
-Meteor.subscribe("users");
 
+
+Meteor.subscribe('users');
 Template.ProfileForm.events({
   'submit form': function(e) {
     e.preventDefault();
 
-    var profile = {
-      id : this.userId,
-      username: $(e.target).find('[name=username]').val(),
-      firstname: $(e.target).find('[name=fname]').val(),
-      lastname: $(e.target).find('[name=lname]').val()
-    };
-
-    Meteor.users.insert(profile);
+  
+	Meteor.users.update({_id:Meteor.user()._id}, {$set: { surname: $(e.target).find('[name=username]').val()
+	/*firstname: $(e.target).find('[name=fname]').val(),
+	lastname: $(e.target).find('[name=lname]').val(),
+    level : 'beginner',
+    accounttype : 'regular'*/
+	}});
     Router.go('postsList');
+  }
+});
+
+Deps.autorun(function(){
+
+	Meteor.subscribe('userData');
+});
+
+Template.userProfil.helpers({
+  users: function() {
+    return Meteor.users.find();
   }
 });
--- server/fixtures.js
+++ server/fixtures.js
@@ -11,6 +11,3 @@ if (Posts.find().count() === 0) {
   
 }
 
-Meteor.users.find({ _id: this.userId },
-    { fields: { username: 1, firstname : 1, lastname :1 } }
-);
